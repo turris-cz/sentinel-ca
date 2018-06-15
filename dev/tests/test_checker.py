0d4b3653bcd7dc178f3b1eadcf3e0d6aad387bc5
Robin Obůrka <robin.oburka@nic.cz>
2018-06-15 16:31:43 +0200

    checker: Provide tests

---
+++ dev/tests/test_checker.py
@@ -0,0 +1,54 @@
+import pytest
+
+import hashlib
+
+import checker
+
+def msg_payload(sn, nonce, good_digest=True):
+    if good_digest:
+        to_hash = "{}:{}".format(sn, nonce)
+        hash_digest = hashlib.sha256(bytes(to_hash, encoding='utf-8'))
+        digest = hash_digest.hexdigest()
+    else:
+        digest = "XXXXXXXXXX"
+
+    msg = {
+        "auth_type": "atsha",
+        "sn": sn,
+        "nonce": nonce,
+        "digest": digest,
+    }
+
+    return msg
+
+
+def test_normal_processing():
+    reply = checker.process("sentinel/certificator/checker", msg_payload("foo", 65487654))
+
+    assert reply["status"] == "ok"
+    assert reply["message"] == ""
+
+
+def test_bad_msg_type():
+    reply = checker.process("sentinel/blabla",  msg_payload("foo", 65487654))
+
+    assert reply["status"] == "failed"
+    assert "Unknown message type" in reply["message"]
+
+
+def test_missing_msg_part():
+    p = msg_payload("foo", 65487654)
+    del p["sn"]
+
+    reply = checker.process("sentinel/certificator/checker", p)
+
+    assert reply["status"] == "failed"
+    assert "missing in the message" in reply["message"]
+
+
+def test_bad_digest():
+    reply = checker.process("sentinel/certificator/checker", msg_payload("foo", 65487654, good_digest=False))
+    print(reply)
+
+    assert reply["status"] == "failed"
+    assert "Provided digest is not valid" in reply["message"]
