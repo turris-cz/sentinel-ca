a539e1dfc9c73ad8d5738572ae92d8660e738202
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2018-06-28 16:42:46 +0200

    ca: Check csr and issue the cert

--- ca.py
+++ ca.py
@@ -5,7 +5,6 @@
 
 import sys
 import datetime
-import hashlib
 import configparser
 import json
 
@@ -19,7 +18,9 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 # signing certs
 from cryptography import x509
+from cryptography.x509.oid import ExtendedKeyUsageOID, NameOID
 from cryptography.hazmat.primitives.asymmetric import ec, rsa
+from cryptography.hazmat.primitives import hashes
 
 
 CONFIG_DEFAULT_PATH = "ca.ini"
@@ -56,6 +57,15 @@ LOG_MESSAGE_MAPPER = {
     "out": "→",
     "none": " ",
 }
+
+SIGNING_HASH = hashes.SHA256()
+ALLOWED_HASHES = {
+    hashes.SHA224,
+    hashes.SHA256,
+    hashes.SHA384,
+    hashes.SHA512,
+}
+CERT_DAYS = 30
 
 
 class CAError(Exception):
@@ -176,6 +186,144 @@ def check_cert(cert, key, ignore_errors):
             sys.exit(2)
 
 
+def load_csr(csr_str):
+    try:
+        csr_data = bytes(csr_str, encoding='utf-8')
+        csr = x509.load_pem_x509_csr(
+                data=csr_data,
+                backend=default_backend()
+        )
+    except (UnicodeEncodeError, ValueError) as e:
+        raise CAError(str(e))
+
+    return csr
+
+
+def check_csr_common_name(csr, identity):
+    common_names = csr.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)
+    if len(common_names) != 1:
+        raise CAError("CSR has not exactly one CommonName")
+
+    common_name = common_names[0].value
+    if common_name != identity:
+        raise CAError("CSR CommonName ({}) does not match desired identity".format(common_name))
+
+def check_csr_hash(csr):
+    h = csr.signature_hash_algorithm
+    if type(h) not in ALLOWED_HASHES:
+        raise CAError("CSR is signed with not allowed hash ({})".format(h.name))
+
+def check_csr_signature(csr):
+    if not csr.is_signature_valid:
+        raise CAError("Request signature is not valid")
+
+def check_csr(csr, device_id):
+    check_csr_common_name(csr, device_id)
+    check_csr_hash(csr)
+    check_csr_signature(csr)
+
+
+def build_subject(identity):
+    return x509.Name([
+        x509.NameAttribute(NameOID.COMMON_NAME, identity),
+    ])
+
+def build_aki(cert):
+    try:
+        ski = cert.extensions.get_extension_for_class(x509.SubjectKeyIdentifier)
+        aki = x509.AuthorityKeyIdentifier.from_issuer_subject_key_identifier(ski)
+
+    except x509.ExtensionNotFound:
+        public_key = cert.public_key()
+        aki = x509.AuthorityKeyIdentifier.from_issuer_public_key(public_key)
+
+    return aki
+
+def build_client_cert(csr, subject, issuer, aki, serial_number=None, days=CERT_DAYS):
+    # generate missing and optional parameters -------
+    not_before = datetime.datetime.utcnow()
+    not_after = datetime.datetime.utcnow() + datetime.timedelta(days=days)
+    if not serial_number:
+        serial_number = x509.random_serial_number()
+
+    # Generate v1 cert (without extensions) ----------
+    cert = x509.CertificateBuilder(
+            issuer_name=issuer,
+            subject_name=subject,
+            public_key=csr.public_key(),
+            serial_number=serial_number,
+            not_valid_before=not_before,
+            not_valid_after=not_after,
+    )
+
+    # Add needed extensions --------------------------
+    # key identifiers
+    cert = cert.add_extension(
+            x509.SubjectKeyIdentifier.from_public_key(csr.public_key()),
+            critical=False
+    )
+    cert = cert.add_extension(
+            aki,
+            critical=False
+    )
+
+    # critical Basic Constraints
+    cert = cert.add_extension(
+            x509.BasicConstraints(
+                ca=False,
+                path_length=None
+            ),
+            critical=True
+    )
+
+    # "Digital Signature" Key Usage
+    cert = cert.add_extension(
+            x509.KeyUsage(
+                digital_signature=True,
+                content_commitment=False,
+                key_encipherment=False,
+                data_encipherment=False,
+                key_agreement=False,
+                key_cert_sign=False,
+                crl_sign=False,
+                encipher_only=False,
+                decipher_only=False
+            ),
+            critical=False
+    )
+
+    # "Client" ExtendedKeyUsage
+    cert = cert.add_extension(
+            x509.ExtendedKeyUsage([ExtendedKeyUsageOID.CLIENT_AUTH]),
+            critical=False
+    )
+
+    return cert
+
+
+def sign_csr(ca_key, ca_cert, csr, identity):
+    cert = build_client_cert(
+            csr=csr,
+            subject=build_subject(identity),
+            issuer=ca_cert.subject,
+            aki=build_aki(ca_cert),
+    )
+
+    cert = cert.sign(ca_key, SIGNING_HASH, default_backend())
+
+    return cert
+
+
+def issue_cert(ca_key, ca_cert, request):
+    device_id = request["sn"]
+    csr = load_csr(request["csr_str"])
+
+    check_csr(csr, device_id)
+    cert = sign_csr(ca_key, ca_cert, csr, device_id)
+
+    return cert.public_bytes(serialization.Encoding.PEM)
+
+
 def init_ca(cert_path, key_path, key_password=None, ignore_errors=False):
     with open(cert_path, 'rb') as f:
         cert = x509.load_pem_x509_certificate(
@@ -214,14 +362,11 @@ def redis_cert_key(request):
     return "{}:{}:{}".format(CERT_KEYSPACE, request["sn"], request["sid"])
 
 
-def issue_cert(request):
-    hash_digest = hashlib.sha256(bytes(request["sn"], encoding='utf-8'))
-    return hash_digest.hexdigest()
+def build_reply(cert_bytes=b'', message=""):
+    cert_str = str(cert_bytes, encoding='utf-8')
 
-
-def build_reply(cert="", message=""):
     return {
-        "cert": cert,
+        "cert": cert_str,
         "message": message,
     }
 
@@ -288,8 +433,8 @@ def main():
         try:
             check_request(request)
             check_auth(socket, request, ctx.args.log_messages)
-            cert = issue_cert(request)
-            reply = build_reply(cert=cert)
+            cert = issue_cert(ca_key, ca_cert, request)
+            reply = build_reply(cert_bytes=cert)
         except CAError as e:
             reply = build_reply(message=str(e))
 
