34d15c61776bbbafdb0d04e1b64bdc67275fdfe1
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2018-07-12 16:36:54 +0200

    db: Use with statement to handle db cursors

--- sentinel_ca/db.py
+++ sentinel_ca/db.py
@@ -2,6 +2,8 @@
 Data storage wrappers for Sentinel:CA
 """
 
+import contextlib
+
 import sqlite3
 
 from .crypto import get_cert_bytes, get_cert_common_name
@@ -33,20 +35,17 @@ def store_cert(db, cert):
     not_after = cert.not_valid_after
     cert_bytes = get_cert_bytes(cert)
 
-    c = db.cursor()
-    c.execute("""
-            INSERT INTO certs(sn, state, common_name, not_before, not_after, cert)
-            VALUES (?,?,?,?,?,?)
-            """,
-            (str(serial_number), "valid", identity, not_before, not_after, cert_bytes)
-    )
-    c.close()
+    with contextlib.closing(db.cursor()) as c:
+        c.execute("""
+                INSERT INTO certs(sn, state, common_name, not_before, not_after, cert)
+                  VALUES (?,?,?,?,?,?)
+                """,
+                (str(serial_number), "valid", identity, not_before, not_after, cert_bytes)
+        )
     db.commit()
 
 
 def row_with_serial_number(db, serial_number):
-    c = db.cursor()
-    c.execute('SELECT * FROM certs WHERE sn=?', (str(serial_number),))
-    row = c.fetchone()
-    c.close()
-    return row
+    with contextlib.closing(db.cursor()) as c:
+        c.execute('SELECT * FROM certs WHERE sn=?', (str(serial_number),))
+        return c.fetchone()
