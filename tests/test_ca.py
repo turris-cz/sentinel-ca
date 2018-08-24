f62796366357ce2277e10bcdc22cfe9d6702177b
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2018-08-24 13:52:59 +0200

    tests: Check CA cert key mismatch

---
+++ tests/test_ca.py
@@ -0,0 +1,39 @@
+"""
+Test CA class
+"""
+
+import pytest
+
+from sentinel_ca.ca import CA
+from sentinel_ca.db import db_connection
+from sentinel_ca.sn import prepare_config
+from sentinel_ca.exceptions import CASetupError
+
+from .crypto_helpers import gen_key, gen_cacert, key_to_bytes, cert_to_bytes
+from .db_helpers import prepare_db
+
+
+def test_ca_key_cert_mismatch(tmpdir):
+    key_path = tmpdir.join("key.pem")
+    cert_path = tmpdir.join("cert.pem")
+    db_path = tmpdir.join("ca.db")
+
+    key = gen_key()
+    wrong_key = gen_key()
+    cert = gen_cacert(wrong_key)
+
+    key_path.write(key_to_bytes(key))
+    cert_path.write(cert_to_bytes(cert))
+
+    prepare_db(str(db_path))
+
+    # apply sentinel_ca config defaults
+    ca_config = prepare_config()
+    # use generated key, cert and db
+    ca_config.set("ca", "cert", str(cert_path))
+    ca_config.set("ca", "key", str(key_path))
+    ca_config.set("db", "path", str(db_path))
+
+    with db_connection(ca_config) as db:
+        with pytest.raises(CASetupError):
+            CA(ca_config, db)
