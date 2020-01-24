c601126c87c3146b54dbd49a4f227dca85598998
Vojtech Myslivec <vojtech.myslivec@nic.cz>
2020-01-24 18:24:18 +0100

    tests: Add test for old request

--- tests/process/errors/test_process.py
+++ tests/process/errors/test_process.py
@@ -3,10 +3,11 @@ Test CA raction to errors end bad requests
 """
 
 import json
+import time
 
 import pytest
 
-from sentinel_ca.__main__ import process
+from sentinel_ca.__main__ import process, VALID_REQUEST_THRESHOLD
 from sentinel_ca.exceptions import CAError
 
 from ...helpers import dict_to_bytes, bytes_to_dict
@@ -28,6 +29,25 @@ def test_process_cacert_expire_soon(redis_mock, good_socket_mock, ca_expire_soon
     # Check certs in sqlite
     csr = csr_from_str(req["csr_str"])
     assert not ca_expire_soon.get_valid_cert_matching_csr(req["sn"], csr)
+
+
+def test_process_old_timestamp(redis_mock, good_socket_mock, ca, good_request):
+    # prepare env
+    req = good_request
+    req["ts"] = int(time.time()) - VALID_REQUEST_THRESHOLD - 1
+    redis_mock.brpop.return_value = (1, dict_to_bytes(req))
+
+    # test
+    process(redis_mock, good_socket_mock, ca)
+
+    # Check SN interaction
+    assert not good_socket_mock.send_multipart.called
+
+    # Check redis interaction
+    assert redis_mock.set.call_count == 1
+    # auth_state
+    auth_state = bytes_to_dict(redis_mock.set.call_args_list[0][0][1])
+    assert auth_state["status"] == "error"
 
 
 def test_process_bad_request_invalid_csr(redis_mock, good_socket_mock, ca, bad_request_invalid_csr):
